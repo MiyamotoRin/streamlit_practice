7b1a930140b9a3a7b593b32685835eac37f61978
MiyamotoRin <67382813+MiyamotoRin@users.noreply.github.com>
2023-02-11 15:02:54 +0900

    fix: Firebaseから安全にデータを受け取る

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds)
 
 # Create a reference to quiz.
 doc_ref = db.collection("quiz").document("1")
